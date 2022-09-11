d945bb4d2b84c990b1ea2390cd7f682f97f4b840
Christopher Seven Phiri <chris7phiri@gmail.com>
2022-09-11 23:44:09 +0200

    Added validation to fields

--- cleaner/mseCleaner.go
+++ cleaner/mseCleaner.go
@@ -15,23 +15,23 @@ import (
 )
 
 type DailyCompanyRate struct {
-	NO         string  `json:"no" required:"true"`
-	HIGH       string  `json:"high" required:"false"`
-	LOW        string  `json:"low" required:"false"` // required true for everything else
-	CODE       string  `json:"code" required:"true"`
-	BUY        float64 `json:"buy" required:"false"`
-	SELL       float64 `json:"sell" required:"false"`
-	PCP        float64 `json:"pcp" required:"true"`
-	TCP        float64 `json:"tcp" required:"true"`
-	VOL        float64 `json:"vol" required:"true"`
-	DIVNET     float64 `json:"div_net" required:"false"`
-	DIVYIELD   float64 `json:"div_yield" required:"false"`
-	EEARNYIELD float64 `json:"earn_yield" required:"true"`
-	PERATIO    float64 `json:"pe_ratio" required:"true"`
-	PBVRATION  float64 `json:"pbv_ratio" required:"true"`
-	CAP        float64 `json:"cap" required:"true"`
-	PROFIT     float64 `json:"profit" required:"true"`
-	SHARES     float64 `json:"shares" required:"true"`
+	NO         string  `json:"no" validate:"string,required=true"`
+	HIGH       float64 `json:"high" validate:"number,required=false"`
+	LOW        float64 `json:"low" validate:"number,required=false"` // required true for everything else
+	CODE       string  `json:"code" validate:"string,required=true"`
+	BUY        float64 `json:"buy" validate:"number,required=false"`
+	SELL       float64 `json:"sell" validate:"number,required=false"`
+	PCP        float64 `json:"pcp" validate:"number,required=true"`
+	TCP        float64 `json:"tcp" validate:"number,required=true"`
+	VOL        float64 `json:"vol" validate:"number,required=true"`
+	DIVNET     float64 `json:"div_net" validate:"number,required=false"`
+	DIVYIELD   float64 `json:"div_yield" validate:"number,required=false"`
+	EEARNYIELD float64 `json:"earn_yield" validate:"number,required=true"`
+	PERATIO    float64 `json:"pe_ratio" validate:"number,required=true"`
+	PBVRATION  float64 `json:"pbv_ratio" validate:"number,required=true"`
+	CAP        float64 `json:"cap" validate:"number,required=true"`
+	PROFIT     float64 `json:"profit" validate:"number,required=true"`
+	SHARES     float64 `json:"shares" validate:"number,required=true"`
 }
 
 type CleanedData struct {
@@ -113,16 +113,12 @@ func Clean(csvFile string, errorPath string, cleanCSVPath string) (*CleanedData,
 		return nil, errors.New(fmt.Sprint(csvFile, err))
 	}
 	var rate DailyCompanyRate
-	for _, word := range records {
+	for i, word := range records {
 		if isInt(word[0]) {
 			if len(word) == 17 {
-				_, err := Verify(word)
-				if err != nil {
-					cleaningErrors = append(cleaningErrors, err.Error())
-				}
 				rate.NO = strings.TrimSpace(word[0])
-				rate.HIGH = strings.TrimSpace(word[1])
-				rate.LOW = strings.TrimSpace(word[2])
+				rate.HIGH = parseFloat(strings.TrimSpace(word[1]))
+				rate.LOW = parseFloat(strings.TrimSpace(word[2]))
 				rate.CODE = strings.TrimSpace(word[3])
 				rate.BUY = parseFloat(strings.TrimSpace(word[4]))
 				rate.SELL = parseFloat(strings.TrimSpace(word[5]))
@@ -137,6 +133,12 @@ func Clean(csvFile string, errorPath string, cleanCSVPath string) (*CleanedData,
 				rate.CAP = parseFloat(strings.TrimSpace(word[14]))
 				rate.PROFIT = parseFloat(strings.TrimSpace(word[15]))
 				rate.SHARES = parseFloat(strings.TrimSpace(word[16]))
+
+				_, err := Verify(rate)
+				if err != nil {
+					cleaningErrors = append(cleaningErrors, fmt.Sprintf("line: %d, number: %q {\n %s \n}", i, word[0], err.Error()))
+				}
+
 				rates = append(rates, rate)
 			}
 		}
@@ -160,7 +162,7 @@ func Clean(csvFile string, errorPath string, cleanCSVPath string) (*CleanedData,
 		w := csv.NewWriter(file)
 		for _, rate := range rates {
 			if err := w.Write([]string{
-				rate.NO, rate.HIGH, rate.LOW, rate.CODE,
+				rate.NO, fmt.Sprintf("%.2f", rate.HIGH), fmt.Sprintf("%.2f", rate.LOW), rate.CODE,
 				fmt.Sprintf("%.2f", rate.BUY), fmt.Sprintf("%.2f", rate.SELL), fmt.Sprintf("%.2f", rate.PCP), fmt.Sprintf("%.2f", rate.TCP), fmt.Sprintf("%.2f", rate.VOL),
 				fmt.Sprintf("%.2f", rate.DIVNET), fmt.Sprintf("%.2f", rate.DIVYIELD), fmt.Sprintf("%.2f", rate.EEARNYIELD),
 				fmt.Sprintf("%.2f", rate.PERATIO), fmt.Sprintf("%.2f", rate.PBVRATION), fmt.Sprintf("%.2f", rate.CAP), fmt.Sprintf("%.2f", rate.PROFIT), fmt.Sprintf("%.2f", rate.SHARES)}); err != nil {
@@ -220,8 +222,7 @@ func parseFloat(value string) float64 {
 
 	f, err := strconv.ParseFloat(value, 64)
 	if err != nil {
-		fmt.Println(err)
-		return 0
+		return -1
 	}
 	return f
 }
@@ -231,21 +232,16 @@ func isInt(value string) bool {
 	return err == nil
 }
 
-func Verify(rate []string) (bool, error) {
-	var err []string
-	isValid := true
-	for i, value := range rate {
-		if i > 2 {
-			if isEmpty(value) {
-				isValid = false
-				err = append(err, fmt.Sprintf("Failed to verify rate, missing fields. \n rate: %s \n null value on: %d", rate, i))
-			}
+func Verify(rate DailyCompanyRate) (bool, error) {
+	err := validateStruct(rate)
+	if len(err) > 0 {
+		var listOfErrors []string
+		for _, x := range err {
+			listOfErrors = append(listOfErrors, x.Error())
 		}
+		return false, errors.New(strings.Join(listOfErrors, "\n"))
 	}
-	if !isValid {
-		return false, errors.New(fmt.Sprint(err))
-	}
-	return isValid, nil
+	return true, nil
 }
 
 func isEmpty(value string) bool {
